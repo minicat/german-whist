e7588222731a8ab366f3ac71ce76c277c9790ee1
Emma Yeap <mintymelody@gmail.com>
2014-11-04 18:12:12 +1100

    you can leave settings via actionbar

--- German Whist/src/main/java/com/minicat/germanwhist/SettingsActivity.java
+++ German Whist/src/main/java/com/minicat/germanwhist/SettingsActivity.java
@@ -1,5 +1,6 @@
 package com.minicat.germanwhist;
 
+import android.app.ActionBar;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
@@ -8,6 +9,8 @@ import android.preference.DialogPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.util.AttributeSet;
+import android.view.MenuItem;
+import android.view.View;
 
 public class SettingsActivity extends PreferenceActivity {
     SharedPreferences mPrefs;
@@ -19,11 +22,38 @@ public class SettingsActivity extends PreferenceActivity {
         // Need to get shared preferences now, so that the resetdialog can access it.
         mPrefs = getSharedPreferences(StatsHelper.PREFS_NAME, 0);
 
+        // want to have the back button there
+        ActionBar actionBar = getActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                // go back
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     public static class ResetDialogPreference extends DialogPreference {
-        public ResetDialogPreference(Context c, AttributeSet attrs) {
-            super(c, attrs);
+        public ResetDialogPreference(Context context, AttributeSet attrs) {
+            super(context, attrs);
+        }
+
+        @Override
+        protected void onDialogClosed(boolean positiveResult) {
+            super.onDialogClosed(positiveResult);
+            persistBoolean(positiveResult);
+        }
+
+        @Override
+        protected void onBindDialogView(View view) {
+            super.onBindDialogView(view);
         }
 
         // Handle OK.
