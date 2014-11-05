8b55121790487d58eb9de33a128208741f6afb34
Emma Yeap <mintymelody@gmail.com>
2014-11-05 15:07:46 +1100

    HelpActivity text. No longer a dialog.

--- German Whist/src/main/java/com/minicat/germanwhist/HelpActivity.java
+++ German Whist/src/main/java/com/minicat/germanwhist/HelpActivity.java
@@ -1,12 +1,32 @@
 package com.minicat.germanwhist;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+// TODO: Add images, trick examples.
 
 public class HelpActivity extends Activity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+
+        // want to have the back button there
+        ActionBar actionBar = getActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                // go back
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
